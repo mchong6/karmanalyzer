fc5d5bf62cede6c4cc1cf3a5607c28c38148acc5
Jason <mchong6@illinois.edu>
2016-11-18 22:36:06 -0600

    fix bugs

--- karma_b.py
+++ karma_b.py
@@ -9,23 +9,20 @@ image_limit = 100
 
 
 imgurUrlPattern = re.compile(r'(http://i.imgur.com/(.*))(\?.*)?')
-targetSubreddit = 'pics'
-if not os.path.exists(targetSubreddit):
-    os.makedirs(targetSubreddit)
 
 
-def downloadImage(imageUrl, localFileName):
+def downloadImage(targetSubreddit, imageUrl, localFileName, count):
     #ignore gifs
     if ".gif" in imageUrl:
-        return
+        return count
     response = requests.get(imageUrl)
     if response.status_code == 200:
-        global count
-        count += 1
         print('Downloading %s...' % (localFileName))
         with open(targetSubreddit+'/'+localFileName, 'wb') as fo:
             for chunk in response.iter_content(4096):
                 fo.write(chunk)
+            return count+1
+
 def get_time(submission):
     time = submission.created_utc
     #time is in UNIX
@@ -34,88 +31,95 @@ def get_time(submission):
 
 # Connect to reddit and download the subreddit front page
 r = praw.Reddit(user_agent='test app mine') # Note: Be sure to change the user-agent to something unique.
-#get top submissions
-submissions = r.get_subreddit(targetSubreddit).get_top_from_day(limit=image_limit)
-#get new submissions
-submissions = chain(submissions, r.get_subreddit(targetSubreddit).get_controversial_from_day(limit=image_limit))
-# Or use one of these functions:
-#                                       .get_top_from_year(limit=25)
-#                                       .get_top_from_month(limit=25)
-#                                       .get_top_from_week(limit=25)
-#                                       .get_top_from_day(limit=25)
-#                                       .get_top_from_hour(limit=25)
-#                                       .get_top_from_all(limit=25)
-
-# Process all the submissions from the front page
-#print vars(next(submissions))
-stats = []
-count = 1
-label = open(targetSubreddit+'/'+targetSubreddit+'.txt', 'w')
-for submission in submissions:
-    # Check for all the cases where we will skip a submission:
-    if "imgur.com/" not in submission.url:
-        continue # skip non-imgur submissions
-    if submission.score < MIN_SCORE:
-        continue # skip submissions that haven't even reached 100 (thought this should be rare if we're collecting the "hot" submission)
-    if len(glob.glob('reddit_%s_%s_*' % (targetSubreddit, submission.id))) > 0:
-        continue # we've already downloaded files for this reddit submission
 
 
-    if 'http://imgur.com/a/' in submission.url:
-        # This is an album submission.
-        albumId = submission.url[len('http://imgur.com/a/'):]
-        htmlSource = requests.get(submission.url).text
 
-        soup = BeautifulSoup(htmlSource, "html.parser")
-        matches = soup.select('.album-view-image-link a')
-        for match in matches:
-            imageUrl = match['href']
+def crawl_sub(targetSubreddit):
+    #create folder if doesnt exist
+    if not os.path.exists(targetSubreddit):
+        os.makedirs(targetSubreddit)
+
+    #get top submissions
+    submissions = r.get_subreddit(targetSubreddit).get_top_from_day(limit=image_limit)
+    #get new submissions
+    submissions = chain(submissions, r.get_subreddit(targetSubreddit).get_controversial_from_day(limit=image_limit))
+    # Or use one of these functions:
+    #                                       .get_top_from_year(limit=25)
+    #                                       .get_top_from_month(limit=25)
+    #                                       .get_top_from_week(limit=25)
+    #                                       .get_top_from_day(limit=25)
+    #                                       .get_top_from_hour(limit=25)
+    #                                       .get_top_from_all(limit=25)
+
+    # Process all the submissions from the front page
+    #print vars(next(submissions))
+    stats = []
+    count = 1
+    label = open(targetSubreddit+'/'+targetSubreddit+'.txt', 'w')
+    for submission in submissions:
+        # Check for all the cases where we will skip a submission:
+        if "imgur.com/" not in submission.url:
+            continue # skip non-imgur submissions
+        if submission.score < MIN_SCORE:
+            continue # skip submissions that haven't even reached 100 (thought this should be rare if we're collecting the "hot" submission)
+        if len(glob.glob('reddit_%s_%s_*' % (targetSubreddit, submission.id))) > 0:
+            continue # we've already downloaded files for this reddit submission
+
+
+        if 'http://imgur.com/a/' in submission.url:
+            # This is an album submission.
+            albumId = submission.url[len('http://imgur.com/a/'):]
+            htmlSource = requests.get(submission.url).text
+
+            soup = BeautifulSoup(htmlSource, "html.parser")
+            matches = soup.select('.album-view-image-link a')
+            for match in matches:
+                imageUrl = match['href']
+                if '?' in imageUrl:
+                    imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+                else:
+                    imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+                #name of image
+                localFileName = str(submission.subreddit) + str(count)
+                count  = downloadImage(targetSubreddit, 'http:' + match['href'], localFileName, count)
+
+        elif 'http://i.imgur.com/' in submission.url:
+            # The URL is a direct link to the image.
+            mo = imgurUrlPattern.search(submission.url) # using regex here instead of BeautifulSoup because we are pasing a url, not html
+
+            imgurFilename = mo.group(2)
+            if '?' in imgurFilename:
+                # The regex doesn't catch a "?" at the end of the filename, so we remove it here.
+                imgurFilename = imgurFilename[:imgurFilename.find('?')]
+
+            #name of image
+            localFileName = str(submission.subreddit) + str(count)
+            count = downloadImage(targetSubreddit, submission.url, localFileName, count)
+
+        elif 'http://imgur.com/' in submission.url:
+            # This is an Imgur page with a single image.
+            htmlSource = requests.get(submission.url).text # download the image's page
+            soup = BeautifulSoup(htmlSource, "html.parser")
+            if len(soup.select('.image a')) == 0:
+                continue
+            imageUrl = soup.select('.image a')[0]['href']
+            if imageUrl.startswith('//'):
+                # if no schema is supplied in the url, prepend 'http:' to it
+                imageUrl = 'http:' + imageUrl
+            imageId = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('.')]
+
             if '?' in imageUrl:
                 imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
             else:
                 imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+
             #name of image
             localFileName = str(submission.subreddit) + str(count)
-            downloadImage('http:' + match['href'], localFileName)
-
-    elif 'http://i.imgur.com/' in submission.url:
-        # The URL is a direct link to the image.
-        mo = imgurUrlPattern.search(submission.url) # using regex here instead of BeautifulSoup because we are pasing a url, not html
-
-        imgurFilename = mo.group(2)
-        if '?' in imgurFilename:
-            # The regex doesn't catch a "?" at the end of the filename, so we remove it here.
-            imgurFilename = imgurFilename[:imgurFilename.find('?')]
-
-        #name of image
-        localFileName = str(submission.subreddit) + str(count)
-        downloadImage(submission.url, localFileName)
-
-    elif 'http://imgur.com/' in submission.url:
-        # This is an Imgur page with a single image.
-        htmlSource = requests.get(submission.url).text # download the image's page
-        soup = BeautifulSoup(htmlSource, "html.parser")
-        if len(soup.select('.image a')) == 0:
-            continue
-        imageUrl = soup.select('.image a')[0]['href']
-        if imageUrl.startswith('//'):
-            # if no schema is supplied in the url, prepend 'http:' to it
-            imageUrl = 'http:' + imageUrl
-        imageId = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('.')]
-
-        if '?' in imageUrl:
-            imageFile = imageUrl[imageUrl.rfind('/') + 1:imageUrl.rfind('?')]
+            count = downloadImage(targetSubreddit, imageUrl, localFileName, count)
         else:
-            imageFile = imageUrl[imageUrl.rfind('/') + 1:]
+            continue
 
-        #name of image
-        localFileName = str(submission.subreddit) + str(count)
-        downloadImage(imageUrl, localFileName)
-    else:
-        continue
+        #write to label
+        label.write(str(submission.subreddit)+ ' '+str(submission.score)+ ' '+str(get_time(submission)) + '\n')
 
-    #write to label
-    label.write(str(submission.subreddit)+ ' '+str(submission.score)+ ' '+str(submission.over_18)+ ' ' +str(get_time(submission)) + '\n')
-    stats.append([str(submission.subreddit), submission.score, submission.over_18, get_time(submission)])
-
-
+crawl_sub('pics')
