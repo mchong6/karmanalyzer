a3818210d2017d4d1ec64d4009630357686941e3
Jason <mchong6@illinois.edu>
2016-11-18 19:08:31 -0600

    initial

---
+++ karma.py
@@ -0,0 +1,11 @@
+import requests
+import requests.auth
+client_auth = requests.auth.HTTPBasicAuth('EUmae3KLK1aJuQ', 'anhKNWTVLRr_2yLpmHpOQxHJzhw')
+post_data = {"grant_type": "password", "username": "HorriblyGood", "password": "hackathon"}
+headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+token = response.json()["access_token"]
+print token
+headers = {"Authorization": "bearer "+token, "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+response = requests.get("https://oauth.reddit.com/subreddits/popular", headers=headers)
+print response.json()
